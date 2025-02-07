71fa4ff61cb4cc66904bc19f0c87dd01a00b9dc1
Emelie40 <nwabuisichiemelie@gmail.com>
2025-02-07 01:57:19 +0100

    a test modification was made to python code

--- main.py
+++ main.py
@@ -65,25 +65,3 @@ async def classify_number(number: str):
         "digit_sum": sum(int(d) for d in str(num)),
         "fun_fact": get_fun_fact(num)
     }
-
-    from fastapi import HTTPException
-
-@app.get("/api/classify-number")
-async def classify_number(number: str):
-    try:
-        num = int(number)
-    except ValueError:
-        raise HTTPException(status_code=400, detail={"number": number, "error": True})
-
-    properties = ["odd" if num % 2 else "even"]
-    if is_armstrong(num):
-        properties.insert(0, "armstrong")
-
-    return {
-        "number": num,
-        "is_prime": is_prime(num),
-        "is_perfect": is_perfect(num),
-        "properties": properties,
-        "digit_sum": sum(int(d) for d in str(num)),
-        "fun_fact": get_fun_fact(num)
-    }
